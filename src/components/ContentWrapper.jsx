bc6c0a1ab4ab00ee4a0bfbf65c36fae9c68ca9ea
JaiderValencia <janvame05@gmail.com>
2023-05-18 20:17:48 -0500

    migro todo a idioma React

--- src/components/ContentWrapper.jsx
+++ src/components/ContentWrapper.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import Footer from './Footer'
+import TopBar from './TopBar'
+import ContentRowTop from './ContentRowTop'
+
 
 function ContentWrapper(params) {
-    return ()   
+    return (<div id="content-wrapper" className="d-flex flex-column">
+
+    {/* <!-- Main Content --> */}
+    <div id="content">
+
+        {/* <!-- Topbar --> */}
+            <TopBar/>
+        {/* <!-- End of Topbar --> */}
+
+        {/* <!-- Content Row Top --> */}
+            <ContentRowTop/>
+        {/* <!--End Content Row Top--> */}
+    </div>
+    {/* <!-- End of MainContent --> */}
+
+    {/* <!-- Footer --> */}
+        <Footer/>
+    {/* <!-- End of Footer --> */}
+
+</div>)   
 }
 
 export default React.memo(ContentWrapper)
