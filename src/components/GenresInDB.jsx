9018178b4a01d1f644fbb428e697b43dca6ee38b
JaiderValencia <janvame05@gmail.com>
2023-05-23 21:26:49 -0500

    Hago que traiga los generos de la DB

--- src/components/GenresInDB.jsx
+++ src/components/GenresInDB.jsx
@@ -1,86 +1,40 @@
 import React from 'react'
 
 class GenresInDB extends React.Component {
+    state = {
+        genresDB: [],
+        overTitleBool: false
+    }
+
+    async componentDidMount() {
+        const genres = await fetch("http://localhost:3001/api/genres")
+            .then(res => res.json())
+
+        this.setState({ genresDB: genres.data })
+    }
+
+    overTitle() {
+        this.setState({ overTitleBool: !this.state.overTitleBool })
+    }
+
     render() {
         return (
             <div className="col-lg-6 mb-4">
                 <div className="card shadow mb-4">
                     <div className="card-header py-3">
-                        <h5 className="m-0 font-weight-bold text-gray-800">Genres in Data Base</h5>
+                        <h5 className={`m-0 font-weight-bold text-gray-800 ${this.state.overTitleBool ? 'bg-secondary' : null}`} onMouseOver={() => this.overTitle()}>Genres in Data Base</h5>
                     </div>
                     <div className="card-body">
                         <div className="row">
-                            <div className="col-lg-6 mb-4">
-                                <div className="card bg-dark text-white shadow">
-                                    <div className="card-body">
-                                        Acción
+                            {this.state.genresDB.map((genre, i) => (
+                                <div className="col-lg-6 mb-4" key={i}>
+                                    <div className="card bg-dark text-white shadow">
+                                        <div className="card-body">
+                                            {genre.name}
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div className="col-lg-6 mb-4">
-                                <div className="card bg-dark text-white shadow">
-                                    <div className="card-body">
-                                        Animación
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-6 mb-4">
-                                <div className="card bg-dark text-white shadow">
-                                    <div className="card-body">
-                                        Aventura
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-6 mb-4">
-                                <div className="card bg-dark text-white shadow">
-                                    <div className="card-body">
-                                        Ciencia Ficción
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-6 mb-4">
-                                <div className="card bg-dark text-white shadow">
-                                    <div className="card-body">
-                                        Comedia
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-6 mb-4">
-                                <div className="card bg-dark text-white shadow">
-                                    <div className="card-body">
-                                        Documental
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-6 mb-4">
-                                <div className="card bg-dark text-white shadow">
-                                    <div className="card-body">
-                                        Drama
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-6 mb-4">
-                                <div className="card bg-dark text-white shadow">
-                                    <div className="card-body">
-                                        Fantasia
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-6 mb-4">
-                                <div className="card bg-dark text-white shadow">
-                                    <div className="card-body">
-                                        Infantiles
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-6 mb-4">
-                                <div className="card bg-dark text-white shadow">
-                                    <div className="card-body">
-                                        Musical
-                                    </div>
-                                </div>
-                            </div>
-
+                            ))}
                         </div>
                     </div>
                 </div>
